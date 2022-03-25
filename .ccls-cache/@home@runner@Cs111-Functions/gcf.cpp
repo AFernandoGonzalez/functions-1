50d22fdf1074f6cb9afa65ab4c0dd2df04a3c1a4
Fernando Gonzalez <58346389+AFernandoGonzalez@users.noreply.github.com>
2022-03-25 13:30:45 +0000

    gcf.cpp program finished

--- .ccls-cache/@home@runner@Cs111-Functions/gcf.cpp
+++ .ccls-cache/@home@runner@Cs111-Functions/gcf.cpp
@@ -3,3 +3,25 @@
 
 using namespace std;
 
+int gcf(int a, int b){
+  if (b == 0){
+    return a;
+  }
+  else {
+    return gcf(b, a%b);
+  }
+}
+
+
+int main(){
+
+  int a = 0, b = 0;
+
+  cout << "Enter a: ";
+  cin >> a;
+  cout << "Enter b: ";
+  cin >> b;
+  cout << "GCF = " << gcf(a,b) << endl;
+  
+  return 0;
+}
--- gcf.cpp
+++ gcf.cpp
@@ -3,3 +3,26 @@
 
 using namespace std;
 
+int gcf(int a, int b){
+  if (b == 0){
+    return a;
+  }
+  else {
+    cout << "Else: " << b << " " << a%b << endl;
+    return gcf(b, a%b);
+  }
+}
+
+
+int main(){
+
+  int a = 0, b = 0;
+
+  cout << "Enter a: ";
+  cin >> a;
+  cout << "Enter b: ";
+  cin >> b;
+  cout << "GCF = " << gcf(a,b) << endl;
+  
+  return 0;
+}
